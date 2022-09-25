20177837d0f3797a55247c2dbb30a60a8b8733c2
wangtao <lionel_tao@163.com>
2022-09-25 23:18:33 +0800

    更新脚本

--- juejin-auto.js
+++ juejin-auto.js
@@ -29,26 +29,31 @@
 */
 
 // 今日是否已签到接口
-let checkTodayStatus = 'https://api.juejin.cn/growth_api/v1/get_today_status'
 
-    (function () {
 
-        // 查询今日是否已签到
-        const checkSignInStatus = async () => {
-            let options = {
-                method: 'GET',
-                headers: {
-                },
-            }
+(function () {
+    let checkTodayStatus = 'https://api.juejin.cn/growth_api/v1/get_today_status'
+    // 查询今日是否已签到
+    const checkSignInStatus = async () => {
+        let options = {
+            method: 'GET',
+            headers: {
+            },
+        }
+        try {
             const res = await fetch(checkTodayStatus, options)
             console.log('------------', res);
+        } catch (error) {
+            console.log('2222', error);
         }
-        checkSignInStatus()
-        // 签到
 
-        // 查询今日是否有免费抽奖机会
+    }
+    checkSignInStatus()
+    // 签到
+
+    // 查询今日是否有免费抽奖机会
 
 
-        // 抽奖
+    // 抽奖
 
-    })()
+})()
