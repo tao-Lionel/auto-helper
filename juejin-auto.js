4abf2f9ef9c59938f4fd15bede97b93aa23abdf2
wangtao <lionel_tao@163.com>
2022-09-27 18:53:22 +0800

    添加掘金自动签到和抽奖

--- juejin-auto.js
+++ juejin-auto.js
@@ -1,76 +1,87 @@
-/* 
-* 今日是否已签到
-*  https://api.juejin.cn/growth_api/v1/get_today_status
-* GET
+/*
+ * 今日是否已签到
+ *  https://api.juejin.cn/growth_api/v1/get_today_status
+ * GET
  */
 
+/*
+ * 签到接口
+ * /growth_api/v1/check_in
+ * POST
+ */
 
-/* 
-* 签到接口
-* /growth_api/v1/check_in
-* POST
-*/
+/*
+ * 查询今日免费抽奖机会
+ * 	/growth_api/v1/lottery_config/get
+ * GET
+ *
+ */
 
-/* 
-* 查询今日免费抽奖机会
-* 	/growth_api/v1/lottery_config/get
-* GET
-*
-*/
+/*
+ * 抽奖
+ *	/growth_api/v1/lottery/draw
+ * GET
+ */
 
-/* 
-* 抽奖
-*	/growth_api/v1/lottery/draw
-* GET
-*/
-
-/* 
-* cookie 
-*/
-
-// 今日是否已签到接口
-
-
-import got from 'got';
+import got from "got";
 (function () {
-    let checkTodayStatusUrl = 'https://api.juejin.cn/growth_api/v1/get_today_status'
+  let checkTodayStatusApi = "https://api.juejin.cn/growth_api/v1/get_today_status";
+  let signInApi = "https://api.juejin.cn/growth_api/v1/check_in";
+  let checkFreeApi = "https://api.juejin.cn/growth_api/v1/lottery_config/get";
+  let drawApi = "https://api.juejin.cn/growth_api/v1/lottery/draw";
+  let cookie_val =
+    "";
+  const HEADERS = {
+    cookie: cookie_val,
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+  };
+  const options = {
+    hooks: {
+      beforeRequest: [
+        options => {
+          Object.assign(options.headers, HEADERS);
+        },
+      ],
+    },
+  };
 
-
-    // 查询今日是否已签到
-    const checkStatus = async () => {
-        let cookie_val = ''
-
-        // const options = {
-        //     method: 'GET',
-        //     cookieJar
-        // }
-
-        const HEADERS = {
-            cookie: cookie_val,
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67'
-        }
-
-        const data = await got('https://api.juejin.cn/growth_api/v1/get_today_status', {
-            hooks: {
-                beforeRequest: [
-                    options => {
-                        Object.assign(options.headers, HEADERS)
-                    }
-
-                ]
-            }
-        }).json()
-        console.log(data);
-
+  // 查询今日是否已签到
+  const checkStatus = async () => {
+    const { err_no, err_msg, data } = await got(checkTodayStatusApi, options).json();
+    console.log("是否签到", data);
+    // 如果没签到去签到
+    if (!data && err_msg === "success") {
+      signIn();
     }
-    checkStatus()
+  };
 
+  // 签到
+  const signIn = async () => {
+    const { err_no, err_msg, data } = await got.post(signInApi, options).json();
+    console.log("签到得的矿石", data.incr_point); // 签到得的矿石
+    console.log("一共有多少矿石", data.sum_point); // 一共有多少矿石
+    if (err_msg === "success") {
+      checkFree();
+    }
+  };
 
-    // 签到
+  // 查询今日是否有免费抽奖机会
+  const checkFree = async () => {
+    const { err_no, err_msg, data } = await got(checkFreeApi, options).json();
+    console.log("免费抽奖机会", data.free_count); // 签到得的矿石
+    if (data.free_count >= 1 && err_msg === "success") {
+      draw();
+    }
+  };
 
-    // 查询今日是否有免费抽奖机会
+  // 抽奖
+  const draw = async () => {
+    const { err_no, err_msg, data } = await got(drawApi, options).json();
+    if (err_msg === "success") {
+      console.log("抽奖成功", data);
+      console.log('抽中的奖品', data.lottery_name);
+    }
+  };
 
-
-    // 抽奖
-
-})()
+  checkStatus();
+})();
