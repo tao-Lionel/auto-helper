338395d54cf80f5439f09ce90a01184a201fc060
wangtao <lionel_tao@163.com>
2023-05-10 11:23:48 +0800

    撤销

--- juejin-auto.js
+++ juejin-auto.js
@@ -172,8 +172,7 @@ import { USER_ID, TEMPLATE_ID } from "./ENV.js";
       template_id: TEMPLATE_ID,
       url: "",
       topcolor: "#2C68FF",
-      // data: _message
-      data: { userName: { value: "喝水运动员", color: "" }, checkMsg: { value: "今日已签到", color: "#E37815" } }
+      data: _message
     };
 
     // 发送消息
